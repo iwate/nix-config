6fc854dae61760dce19af0e9d5834c2a4fb32309
iwate <github@iwate.me>
2026-05-01 16:08:41 +0900

    remove unused RDP connection script and related environment variables

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,27 +1,4 @@
 { config, pkgs, inputs, ... }:
-let
-  # 接続用スクリプトを定義
-  rdpScript = pkgs.writeShellScriptBin "connect-work-rdp" ''
-    # 1Password CLI とデスクトップアプリを連携させるための環境変数
-    export OP_BIOMETRIC_UNLOCK_ENABLED=true
-    # ソケットのパスを明示的に指定（NixOSのデフォルト）
-    #export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
-
-    
-    PASS=$(${pkgs._1password-cli}/bin/op read op://Private/RDP/password --debug)
-    HOST=$(${pkgs._1password-cli}/bin/op read op://Private/RDP/host)
-    USER=$(${pkgs._1password-cli}/bin/op read op://Private/RDP/username)
-    DOMAIN=$(${pkgs._1password-cli}/bin/op read op://Private/RDP/domain)
-    GW=$(${pkgs._1password-cli}/bin/op read op://Private/RDP/gateway)
-
-    echo "$PASS" | ${pkgs.freerdp}/bin/xfreerdp /from-stdin \
-      /v:"$HOST" \
-      /u:"$USER" \
-      /d:"$DOMAIN" \
-      /gateway:g:"$GW" \
-      /f /kbd:layout:Japanese /kbd:remap:0x15d=0x64 /scale:140
-  '';
-in
 {
   programs.home-manager.enable = true;
   home.stateVersion = "25.11"; 
