e66888b7d4ed5e8141493440cb7f99835fbc4410
iwate <github@iwate.me>
2026-05-06 23:47:56 +0900

    Remove unstable channel from nix flake update command in check-updates.sh

--- check-updates.sh
+++ check-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nix flake update nixpkgs nixpkgs-unstable
+nix flake update nixpkgs
 
 if git diff flake.lock | grep -q .; then
   notify-send -u critical -i software-update-available "システム更新" "新しいアップデートが利用可能です。"
