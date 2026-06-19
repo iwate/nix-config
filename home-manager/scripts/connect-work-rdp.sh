40a200e4d186e38951d01e4f0d520a45d3d749b1
iwate <github@iwate.me>
2026-06-20 00:44:10 +0900

    Add setxkbmap command for Japanese keyboard layout in RDP script

--- home-manager/scripts/connect-work-rdp.sh
+++ home-manager/scripts/connect-work-rdp.sh
@@ -6,10 +6,11 @@ USER=$(op read op://Private/RDP/username)
 DOMAIN=$(op read op://Private/RDP/domain)
 GW=$(op read op://Private/RDP/gateway)
 
+DISPLAY=:0 setxkbmap jp
+
 echo "$PASS" | DISPLAY=:0 xfreerdp /from-stdin \
 /v:"$HOST" \
 /u:"$USER" \
 /d:"$DOMAIN" \
 /gateway:g:"$GW" \
 /f /kbd:layout:Japanese /kbd:remap:0x3a=0x64 /scale:140
-        
